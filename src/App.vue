f9d5367d6ae04e86bb1f0070b5d3c5d20805d368
joydumply <nick.babichev@gmail.com>
2023-12-07 23:56:13 +0200

    implemented pagination;
    pagination with query URL in progress

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
-import { onMounted, ref, toRefs } from 'vue';
+import { computed, onMounted, onUpdated, ref, toRefs, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useMoviesStore } from './stores/movies';
 import MoviesList from './components/MoviesList.vue';
 import PosterBg from './components/PosterBg.vue';
+import MoviesPagintion from './components/MoviesPagintion.vue';
 const moviesStore = useMoviesStore();
+
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const getUrlQueryParams = async () => {
+	await router.isReady();
+	console.log(route.query);
+	return route.query
+};
 
 const moviesData = toRefs(moviesStore);
 const moviesList = moviesData.movies;
@@ -15,9 +27,60 @@ const onChangePoster = (poster) => {
 	posterBg.value = poster;
 };
 
+const currentPage = computed({
+	get() {
+		return moviesData.currentPage.value;
+	},
+	set(value) {
+		moviesData.currentPage.value = value;
+	},
+});
+const moviesPerPage = computed({
+	get() {
+		return moviesData.moviesPerPage.value;
+	},
+	set(value) {
+		moviesData.moviesPerPage.value = value;
+	},
+});
+
+const moviesTotal = computed(() => moviesData.getMoviesListLength.value);
+
+const onPageChanged = (page) => {
+	currentPage.value = page;
+	router.push({ query: { page } });
+	moviesStore.fetchMovies();
+};
+
 onMounted(() => {
 	moviesStore.fetchMovies();
+
+	// const pageQuery = getUrlQueryParams();
+
+	getUrlQueryParams().then((v) => {
+		console.log(v);
+	});
+
+	// if (route.query.page) {
+	// 	console.log('I am here');
+	// 	currentPage.value = route.query.page;
+	// 	moviesStore.fetchMovies();
+	// }
+	// console.log(router.currentRoute.query);
+	// if (router.currentRoute.query) {
+	// 	console.log('I am here router');
+
+	// 	currentPage.value = route.query.page;
+	// 	moviesStore.fetchMovies();
+	// }
 });
+
+watch(
+	() => route.query,
+	(newQuery, oldQuery) => {
+		console.log('New query parameters:', newQuery);
+	}
+);
 </script>
 
 <template>
@@ -25,6 +88,13 @@ onMounted(() => {
 	<MoviesList
 		:list="moviesList"
 		@changePoster="onChangePoster"
+	/>
+	{{ moviesTotal.value }}
+	<MoviesPagintion
+		:current-page="currentPage"
+		:total="moviesTotal"
+		:perPage="moviesPerPage"
+		@pageChanged="onPageChanged"
 	/>
 </template>
 
